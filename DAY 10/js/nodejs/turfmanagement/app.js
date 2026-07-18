d0d7dc2a479f85c3aeb64ab6696f40608a36102e
jayanthamara21 <jayanth.amara@prodapt.com>
2026-07-18 19:40:55 +1300

    Add main function to fetch and log bookings

---
+++ DAY 10/js/nodejs/turfmanagement/app.js
@@ -0,0 +1,7 @@
+import { getBookings } from "./api/getBookings.js";
+async function main(){
+    console.log("Fetching latest bookings from JSON Server...\n");
+    const bookings = await getBookings();
+    console.log(bookings);
+}
+main();
